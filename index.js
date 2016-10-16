4c359069cdea5a1065a52249be2c7ae0dbd01761
dnichol <dnichol@newrelic.com>
2016-10-15 21:00:02 -0700

    :moneybag: 'Got this working'

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var emojiOne = require('emojione')
-var http = require('http');
-http.get('http://emoji.getdango.com/api/emoji?q='+process.argv[2], function(res) {
+var https = require('https');
+var str = ''
+// console.log('asdf', process.argv.slice(2).join(' '))
+var req = process.argv.slice(2).join(' ')
+https.get('https://emoji.getdango.com/api/emoji?q='+process.argv[2], function(res) {
   res.on("data", function(chunk) {
-  	var result = chunk && JSON.parse(chunk) && JSON.parse(chunk).results && JSON.parse(chunk).results[0] && JSON.parse(chunk).results[0].text || ':bowtie:'
-    console.log(emojiOne.toShort(result));
+    str += chunk;
+  })
+  res.on('end', function () {
+    var result = str && JSON.parse(str) && JSON.parse(str).results && JSON.parse(str).results[0] && JSON.parse(str).results[0].text || ':bowtie:'
+    console.log(emojiOne.toShort(result))
   });
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
